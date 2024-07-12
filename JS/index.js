ab52a0ad062cd7bbe63ff1c9af6c1314032757bc
Nguyễn Tấn Cường <57739244+tancuong17@users.noreply.github.com>
2024-07-12 19:31:46 +0700

    Update index.js

---
+++ JS/index.js
@@ -0,0 +1,27 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 20
+        },
+        480: {
+          slidesPerView: 1,
+          spaceBetween: 30
+        },
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20
+        }
+      }
+});
